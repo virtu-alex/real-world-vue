d125c1a1198285dc1e2600b15ae8c888e807d501
virtu-alex <alessio.cordari@gmail.com>
2022-08-21 19:46:07 +0200

    Real world vue replica

---
+++ src/views/EventList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>event listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import axios from "axios";
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/events")
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.log("there was an error:" + error.response);
+      });
+  },
+};
+</script>
+
+<style></style>
